1e257bd48e367b9bb0c65f51507d9a497a7f6256
Can Aksakal <aksakalcan97@gmail.com>
2024-08-12 12:23:26 +0300

    some features

---
+++ script.js
@@ -0,0 +1,45 @@
+/* seçildiğinde yeşil border eklesin
+başka bir kart seçildiğinde yeşil border bırakılsın
+üzerine gelindiğinde indirim eklesin */
+
+const cardsDOM = document.querySelectorAll(".cards");
+const chooseButtonDOM = document.querySelectorAll(".choose-button");
+const priceDOM = document.querySelectorAll(".price");
+const priceAreaDOM = document.querySelectorAll(".price-area");
+
+chooseButtonDOM.forEach((button) => {
+  button.addEventListener("click", function () {
+    cardsDOM.forEach((card) => {
+      card.style.border = "none";
+    });
+    button.parentElement.parentElement.style.border = "4px solid green";
+  });
+});
+
+priceDOM.forEach((price) => {
+  //const currentPriceDOM = document.querySelectorAll(".current-price");
+  //const discountDOM = document.createElement("span");
+  price.parentElement.parentElement.parentElement.addEventListener(
+    "mouseenter",
+
+    function (e) {
+      const currentPriceDOM = e.currentTarget.querySelector(".current-price");
+      currentPriceDOM.style.textDecoration = "line-through";
+
+      const discountDOM = document.createElement("span");
+      discountDOM.innerHTML = "<br>140 TL/AY";
+      discountDOM.className = "discount";
+      price.appendChild(discountDOM);
+    }
+  );
+  price.parentElement.parentElement.parentElement.addEventListener(
+    "mouseleave",
+    function (e) {
+      const currentPriceDOM = e.currentTarget.querySelector(".current-price");
+      currentPriceDOM.style.textDecoration = "none";
+
+      const discountDOM = e.currentTarget.querySelector(".discount");
+      price.removeChild(discountDOM);
+    }
+  );
+});
